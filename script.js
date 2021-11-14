8f4c47025c9642e5a33b5d292a10fc9c6cb5160e
Sreenivasan13 <69409059+Sreenivasan13@users.noreply.github.com>
2021-11-14 10:10:05 +0530

    call back hell task

---
+++ script.js
@@ -0,0 +1,74 @@
+
+var myDiv = document.createElement("div");
+myDiv.style.backgroundColor = "black";
+myDiv.style.color = "blue";
+myDiv.style.textAlign = "center";
+myDiv.style.fontSize = "100px";
+document.body.appendChild(myDiv);
+
+
+// setTimeout(function() {  
+//     console.log("This message is shown after 3 seconds");
+// }, 3000);
+// function ten(){
+//     console.log("10");
+// }
+// ten();
+
+// function timeout_trigger() {
+//     window.alert('Happy Birthday..!');   
+// }
+
+// function timeout_init() {
+//     setTimeout('timeout_trigger()', 2000);
+// }
+// timeout_init();
+
+function execute(a, cb){
+    var result = a;
+    cb(result);
+      setTimeout(function(){
+           result = 9;
+           cb(result);
+            setTimeout(function(){
+               result = 8;
+               cb(result);
+                  setTimeout(function(){
+                     result = 7;
+                     cb(result);
+                       setTimeout(function(){
+                          result = 6;
+                          cb(result);
+                          setTimeout(function(){
+                            result = 5;
+                            cb(result);
+                              setTimeout(function(){
+                                  result = 4;
+                                  cb(result);
+                                      setTimeout(function(){
+                                           result = 3;
+                                           cb(result);
+                                                setTimeout(function(){
+                                                      result = 2;
+                                                      cb(result);
+                                                          setTimeout(function(){
+                                                                 result = 1;
+                                                                 cb(result);
+                                                                 setTimeout(function(){
+                                                                    result = "Happy BirthDay...!";
+                                                                    cb(result);
+                                                                }, 10000)
+                                                          }, 9000)
+                                                }, 8000)
+                                      }, 7000)
+                              }, 6000)
+                          }, 5000)
+                       }, 4000)
+                  }, 3000)
+            }, 2000)
+       }, 1000)
+}
+execute(10, function(resultData){
+    console.log(resultData);
+    myDiv.innerText = resultData;
+});
